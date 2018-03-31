f8e0acc900868c0248ba9d375f16ec32fbad439c
tcqefjm <32012341+tcqefjm@users.noreply.github.com>
2018-03-31 15:59:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,30 @@
 from Crypto.Util import number
+from pymongo import MongoClient
 
 class Transaction(object):
     def __init__(self,item_set,item_num=1000):
         self.item=[1 if str(i) in item_set else 0 for i in range(item_num)]
     def encrypt(self,s,a,p):
-        encrypted_transaction=[]
-        for i in self.item:
+        encrypted_transaction={}
+        for i,value in enumerate(self.item):
             r=number.getRandomNBitInteger(100)
-            encrypted_transaction.append(s*(a+r)%p if i else s*r%p)
+            encrypted_transaction['item'+str(i)]=str(s*(a+r)%p if value else s*r%p)
         return encrypted_transaction
 
 s=101010001010100101001011                
 def test():
     with open('prime.dat','r') as g:
         a,p=int(g.readline()),int(g.readline())
+    client=MongoClient('mongodb://172.17.0.2:27017/')#更改为自己mongodb的IP
+    db=client.testdata
+    
     with open('retail.dat','r') as f:
+        tmp=[]
         for times in range(10):
             item_list=f.readline().split()
             test=Transaction(item_list)
-            print(test.encrypt(s,a,p))
+            tmp.append(test.encrypt(s,a,p))
+        #print(tmp)
+        db.encrypted.insert_many(tmp)
 
 test()
