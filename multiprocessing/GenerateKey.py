c33c52f6db520cc45bfe90d0bc01b3d7f619a4eb
tcqefjm <32012341+tcqefjm@users.noreply.github.com>
2018-09-20 09:26:20 +0800

    Update GenerateKey.py

--- multiprocessing/GenerateKey.py
+++ multiprocessing/GenerateKey.py
@@ -2,7 +2,7 @@ from Crypto.Util import number
 from pymongo import MongoClient
 
 def GenerateKey(db):
-    security_parameter={'a':str(number.getPrime(600)),'p':str(number.getPrime(8192)),'s':str(number.getRandomNBitInteger(8190))}
+    security_parameter={'a':str(number.getPrime(100)),'p':str(number.getPrime(1024)),'s':str(number.getRandomNBitInteger(1020))}
     db.security_parameter.insert_one(security_parameter)
 
 GenerateKey(MongoClient('mongodb://127.0.0.1:27017/').user)
