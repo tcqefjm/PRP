4ba2ccef4b1c4e93fa75580696a136d2dece0543
tcqefjm <32012341+tcqefjm@users.noreply.github.com>
2018-09-23 17:09:03 +0800

    Delete GenerateKey.py

--- multiprocessing/GenerateKey.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Util import number
-from pymongo import MongoClient
-
-def GenerateKey(db):
-    security_parameter={'a':str(number.getPrime(100)),'p':str(number.getPrime(1024)),'s':str(number.getRandomNBitInteger(1020))}
-    db.security_parameter.insert_one(security_parameter)
-
-GenerateKey(MongoClient('mongodb://127.0.0.1:27017/').user)
